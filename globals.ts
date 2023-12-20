6188dd7cf523673113c2dbe631b1b030067abea9
Manan Patel <manan77patel@gmail.com>
2023-12-20 16:03:53 +0100

    Update globals.ts

--- globals.ts
+++ globals.ts
@@ -1,6 +1,6 @@
 // Network Connections
-export const RPC_URL_TESTNET = 'https://rpc.testnet.lukso.gateway.fm';
-export const RPC_URL_MAINNET = 'https://rpc.lukso.gateway.fm';
+export const RPC_URL_TESTNET = 'https://rpc-mumbai.maticvigil.com';
+export const RPC_URL_MAINNET = 'https://polygon-rpc.com';
 
 // Data Source
-export const LUKSO_IPFS_BASE_URL = 'https://2eff.lukso.dev/ipfs/';
+export const LUKSO_IPFS_BASE_URL = 'https://desights-dev.myfilebase.com/ipfs/';
