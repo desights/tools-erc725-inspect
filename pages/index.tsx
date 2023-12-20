22479deef91eabfd3fd1790b3d2e03fbffe2a8ec
Manan Patel <manan77patel@gmail.com>
2023-12-20 22:05:29 +0100

    updated footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,6 +63,8 @@ const Home: NextPage = () => {
               },
             ]}
           ></CardContainer>
+          <br></br>
+          <br></br>
           <p>
             Forked with ❤️ from{' '}
             <a
